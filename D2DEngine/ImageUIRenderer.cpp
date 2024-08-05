b465bc6e8590840c3e05e9f1f190308bcc09cb8a
Hohyun.Kim <hohyun1022@naver.com>
2024-08-05 19:48:17 +0900

    Add ImageUIRenderer

---
+++ D2DEngine/ImageUIRenderer.cpp
@@ -0,0 +1,84 @@
+#include "pch.h"
+#include "ImageUIRenderer.h"
+#include "AABB.h"
+#include "Transform.h"
+#include "GameObject.h"
+#include "Texture.h"
+#include "ResourceManager.h"
+
+ImageUIRenderer::ImageUIRenderer()
+{
+}
+
+ImageUIRenderer::~ImageUIRenderer() {
+	if (m_pTexture) {
+		ResourceManager::GetInstance().ReleaseAnimationAsset(m_strTextureFilePath);
+		m_pTexture = nullptr;
+	}
+}
+
+void ImageUIRenderer::LoadTexture(const std::wstring strFilePath)
+{
+	m_strTextureFilePath = strFilePath;
+
+	if (ResourceManager::GetInstance().CreateTextureFromFile(m_strTextureFilePath, &m_pTexture))
+	{
+		auto size = m_pTexture->m_pD2DBitmap->GetSize();
+		m_DstRect.left = 0.f;
+		m_DstRect.top = 0.f;
+		m_DstRect.right = size.width;
+		m_DstRect.bottom = size.height;
+		m_SrcRect = m_DstRect;
+
+		m_ImageTransform = D2D1::Matrix3x2F::Scale(1.0f, 1.0f, D2D1::Point2F(0, 0)) *
+			D2D1::Matrix3x2F::Translation(size.width * -1 / 2.f, size.height / 2.f);
+	}
+}
+
+void ImageUIRenderer::Update(float deltaTime)
+{
+}
+
+void ImageUIRenderer::Render(ID2D1HwndRenderTarget* pRenderTarget, D2D1_MATRIX_3X2_F cameraMat)
+{
+	if (gameObject->isActive == false || (gameObject->transform->m_pParentScene != nullptr && gameObject->transform->m_pParentScene->gameObject->isActive == false))
+		return;
+	D2D1_MATRIX_3X2_F m_ScreenTransform =
+		D2D1::Matrix3x2F::Scale(1.0f, -1.0f) *
+		D2D1::Matrix3x2F::Translation(640.f, 360.f);
+	D2D1_MATRIX_3X2_F Transform =
+		D2D1::Matrix3x2F::Scale(1.0f, -1.0f) * m_ImageTransform
+		* gameObject->transform->m_WorldTransform
+		* cameraMat
+		* m_ScreenTransform;
+	;// * D2DRenderer::m_CameraTransform;
+	pRenderTarget->SetTransform(Transform);
+	pRenderTarget->DrawBitmap(m_pTexture->m_pD2DBitmap, m_DstRect, 1.0f, D2D1_BITMAP_INTERPOLATION_MODE_NEAREST_NEIGHBOR, m_SrcRect);
+}
+
+void ImageUIRenderer::Render(D2D1_MATRIX_3X2_F cameraMat)
+{
+	if (gameObject->isActive == false || (gameObject->transform->m_pParentScene != nullptr && gameObject->transform->m_pParentScene->gameObject->isActive == false))
+		return;
+	auto pRenderTarget = &D2DRenderer::getRenderTarget();
+
+	D2D1_MATRIX_3X2_F m_ScreenTransform =
+		D2D1::Matrix3x2F::Scale(1.0f, -1.0f) *
+		D2D1::Matrix3x2F::Translation(0.f, 720.f);
+	D2D1_MATRIX_3X2_F Transform =
+		D2D1::Matrix3x2F::Scale(1.0f, -1.0f) * m_ImageTransform
+		* gameObject->transform->m_WorldTransform
+		* cameraMat
+		* m_ScreenTransform;
+	;// * D2DRenderer::m_CameraTransform;
+	pRenderTarget->SetTransform(Transform);
+	pRenderTarget->DrawBitmap(m_pTexture->m_pD2DBitmap, m_DstRect, 1.0f, D2D1_BITMAP_INTERPOLATION_MODE_NEAREST_NEIGHBOR, m_SrcRect);
+}
+
+AABB ImageUIRenderer::GetBound()
+{
+	AABB ab;
+	ab.SetCenter(gameObject->transform->m_WorldTransform.dx, gameObject->transform->m_WorldTransform.dy);
+	ab.SetExtent(float((m_DstRect.right - m_DstRect.left) / 2), float((m_DstRect.bottom - m_DstRect.top) / 2));
+	return ab;
+}
