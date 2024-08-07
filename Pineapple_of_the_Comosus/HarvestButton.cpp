843901eea30b2fdc6e18fba8380f80034763f43e
Hohyun.Kim <hohyun1022@naver.com>
2024-08-07 12:05:24 +0900

    기본 Ui 배치 완료, 수확 시스템 추가
    기본 Ui 배치 완료, 수확 시스템 추가

--- Pineapple_of_the_Comosus/HarvestButton.cpp
+++ Pineapple_of_the_Comosus/HarvestButton.cpp
@@ -2,38 +2,39 @@
 #include "Mpbar.h"
 
 HarvestButton::HarvestButton()
-    : HarvestMp(0.0f), maxExperience(100.0f), isHarvestable(false)
+    : HarvestMp(0.0f), maxExperience(100.0f)
 {
 }
 
-void HarvestButton::Harvest()
+
+// 수확 가능여부 체크 function
+bool HarvestButton::HarvestAble()
 {
-    if (isHarvestable)
+    if (mp->getCurrMp() >= mp->getMaxMp() * harvestMinmumGage)
     {
-        // 수확 로직 구현
+        return true;
     }
+    return false;
 }
 
 void HarvestButton::UpdateButtonOpacity()
 {
     // 버튼의 Opacity를 설정하는 로직
     // opacity 값의 범위는 0 (완전히 투명)에서 1 (완전히 불투명)까지
-    float opacity = isHarvestable ? 1.0f : 0.0f; // 수확 가능 여부에 따라 opacity 조정
+    float opacity = HarvestAble() ? 1.0f : 0.0f; // 수확 가능 여부에 따라 opacity 조정
 
     // 버튼의 UI 컴포넌트에서 opacity 값을 설정하는 메서드 호출
     // 예: buttonUI->SetOpacity(opacity); 또는 buttonUI->opacity = opacity;
     // 버튼 UI 컴포넌트의 종류에 따라 다를 수 있습니다.
+    // 여기서 수확 버튼 동작 
 }
 
 void HarvestButton::Update(float deltaTime)
 {
     // 경험치를 확인하고 수확 가능 여부를 설정
-    if (mp)
+    if (HarvestAble())
     {
-        float currentExperience = mp->getCurrMp(); // Mpbar의 현재 경험치를 가져오는 함수
-        isHarvestable = (currentExperience >= maxExperience * 0.75f);
-
-        // 버튼의 투명도 업데이트
+        // 버튼의 투명도 업데이트 -> 수확 버튼 활성화
         UpdateButtonOpacity();
     }
 }
--- Pineapple_of_the_Comosus/HarvestButton.h
+++ Pineapple_of_the_Comosus/HarvestButton.h
@@ -2,22 +2,25 @@
 #include "../D2DEngine/Component.h"
 
 class Mpbar; // 선언
+class ImageUIRenderer;
 class HarvestButton : public Component
 {
 private:
     float HarvestMp; // 수확에 필요한 경험치
     float maxExperience; // 최대 경험치
-    bool isHarvestable; // 수확 가능 여부
+    float harvestMinmumGage = 0.75f;
 
     // 수확 가능구간 변수
     void UpdateButtonOpacity(); // 버튼의 투명도 업데이트
 public:
+
+    ImageUIRenderer* ImageRender;
     HarvestButton();
 
     Mpbar* mp; // Mpbar 객체에 대한 포인터
 
-    // 수확하기 - 버튼 클릭시 동작
-    void Harvest();
+    // 수확가능 여부 체크 
+    bool HarvestAble();
 
     // 버튼 상태 업데이트
     void Update(float deltaTime);
