9945db5c554913398c18b84ab2f92a6a1155375f
Hohyun.Kim <hohyun1022@naver.com>
2024-08-09 17:39:30 +0900

    코드 수정
    코드 수정

--- Pineapple_of_the_Comosus/MainPineApple.cpp
+++ Pineapple_of_the_Comosus/MainPineApple.cpp
@@ -9,9 +9,9 @@ void MainPineApple::PrintIndex(int index)
 
 void MainPineApple::Update(float deltaTime)
 {
-	elapsedTime += deltaTime;
-	float temp = std::sinf(elapsedTime * 10.f) * 30.f;
-	//std::cout << temp << std::endl;
-	gameObject->transform->pos.worldPosition.y = temp;
-	//gameObject->transform->m_RelativeRotation = elapsedTime * 30.f;
+	//elapsedTime += deltaTime;
+	//float temp = std::sinf(elapsedTime * 10.f) * 30.f;
+	////std::cout << temp << std::endl;
+	//gameObject->transform->pos.worldPosition.y = temp;
+	////gameObject->transform->m_RelativeRotation = elapsedTime * 30.f;
 }
