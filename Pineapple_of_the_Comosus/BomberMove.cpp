5c4295241837ac42b1a1f97a1f15384e95b43e74
HamHuman009 <166357862+HamHuman009@users.noreply.github.com>
2024-08-21 11:57:37 +0900

    폭탄 카트 제자리에서 터지게 함

--- Pineapple_of_the_Comosus/BomberMove.cpp
+++ Pineapple_of_the_Comosus/BomberMove.cpp
@@ -3,14 +3,21 @@
 #include "../D2DEngine/SoundManager.h"
 #include "../D2DEngine/Movement.h"
 #include "../D2DEngine/SpriteAnimation.h"
-
+#include "../D2DEngine/GameObject.h"
+#include "../D2DEngine/Transform.h"
 void BomberMove::Enter()
 {
+	enemy = fsm->gameObject->GetComponent<Enemy>();
 }
 
 void BomberMove::Update(float deltaTime)
 {
 	moveTime += deltaTime;
+	if (abs(fsm->gameObject->transform->pos.worldPosition.x) <
+		0/*파인애플 좌표*/ + 300/*파인애플 너비의 반*/ + enemy->enemyData.range)
+	{
+		fsm->SetState("Attack");
+	}
 	if (moveTime > 2.f)
 	{
 		SoundManager::GetInstance().PlaySoundW(L"BombCartMovement_Se", false);
@@ -31,4 +38,5 @@ void BomberMove::Update(float deltaTime)
 
 void BomberMove::Exit()
 {
+	enemy->move->m_speed = 0.f;
 }
