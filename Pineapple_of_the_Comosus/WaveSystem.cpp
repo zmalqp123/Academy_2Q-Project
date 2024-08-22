24218611caf9b3fae83c21675d9c8e451472ba1e
HamHuman009 <166357862+HamHuman009@users.noreply.github.com>
2024-08-22 14:28:02 +0900

    튜토리얼 더 고침

--- Pineapple_of_the_Comosus/WaveSystem.cpp
+++ Pineapple_of_the_Comosus/WaveSystem.cpp
@@ -458,7 +458,7 @@ void WaveSystem::Update(float deltaTime)
         PushingHowManyDay(currentWave);
     }
 
-    if (waveTimer <= 10.f) {
+    if (currentWave >= 5 && waveTimer <= 10.f) {
 		elapsedTime2 += deltaTime;
         PullingHowManyDay(currentWave);
         
